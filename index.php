2f278b7bd13104135370d3baf417ccf4c7069e39
siranno <yoanndelaloy45@gmail.com>
2018-08-05 18:10:59 +0200

    instalation du site portfolio

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include 'header.php'; ?>
+	<title>Yoann DELALOY</title>
+</head>
+<body>
+	<?php include 'menufixe.php'; ?>
+	<br>
+	<div class="row">
+	<?php include 'menuv.php'; ?>
+    <div class="col-sm-9 col-md-9 affix-content" style="background-color: lightgray; border-width:2px; border-style:outset; border-radius: 15px;  box-shadow:10px 10px 20px 0px #696969;">
+      <div class="container">
+        <div class="col-md-12">
+          <div class="page-header" style="text-align: center;">
+            <h3 style="text-shadow: 5px 5px 6px #82576b;"><span class="glyphicon glyphicon-th-list"></span><strong>Mon portfolio</strong></h3>
+          </div><br />
+          <p style="text-align: center; background-color: #FFFEFE; border-width:2px; border-style:outset; border-radius: 15px;  box-shadow:10px 10px 20px 0px #696969; ">
+            Bienvenue sur mon <strong>portfolio</strong>,<br /><br />
+            Ce site complète mon <strong>CV</strong> et <strong>lettre de motivation</strong>, je vous présente mes études et formation, mes expériences et mes projets. Vous y trouverez des détails sur chacune de mes expériences et formation, avec des illustrations et des vidéos de présentation. Cela vous permettra d'en savoir plus sur moi et de savoir si je correspond aux <strong>profil que vous recherchez</strong>. Vous avez également accès à tous mes projets comme la <strong>vitrine, le blog et le site vitrine</strong> qui sont témoins de certaines de mes compétences. Vous y trouverez également mes diplômes et certification de chaque formation effectués. <br /><a href="contact.php" title="Yoann Contact" style="color: #82576b;"><strong>Me Contacter</strong></a></p>
+        </div>
+        <br />
+        
+        <div class="row">
+          <div class="col-md-4">
+            <div class="col-md-12" style="background-color: #FFFEFE; border-width:2px; border-style:outset; border-radius: 15px;  box-shadow:10px 10px 20px 0px #696969;">
+              <div class="page-header" style="text-align: center;">
+                <h3 style="text-shadow: 5px 5px 6px #82576b;"><span class="glyphicon glyphicon-th-list"></span><strong>Mes formations</strong></h3>
+              </div>
+              <br />
+              <img src="https://image.ibb.co/kJ45kS/25.jpg" class="img-fluid" alt="Bureaux création">
+              <br /><br />
+              <p style="text-align: center;">Dans cette rubrique vous y trouverez mes formations, mes études et mes compétences. Vous y trouverez des détails et des illustrations sur : mon <strong>BEP carrosserie automobile, mon apprentissage mécanique, ma formation de maçon aux compagnons du devoir, mes formations de trader et mes formations autour du numérique</strong>. 
+               <br /><a href="formation.php" style="color: #82576b;"><strong>En savoir plus</strong></a></p>
+            </div><br>
+          </div>
+          <br />
+          <div class="col-md-4">
+            <div class="col-md-12" style="background-color: #FFFEFE; border-width:2px; border-style:outset; border-radius: 15px;  box-shadow:10px 10px 20px 0px #696969;">
+              <div class="page-header" style="text-align: center;">
+                <h3 style="text-shadow: 5px 5px 6px #82576b;"><span class="glyphicon glyphicon-th-list"></span><strong>A propos</strong></h3>
+              </div>
+              <br />
+              <img src="https://image.ibb.co/hA7ms7/40.jpg" class="img-fluid" alt="A Propos de moi">
+              <br /><br />    
+              <p style="text-align: center;">Vous souhaitez en savoir plus sur moi ? C'est bien ici, vous saurez alors pour quelle raison je suis motivé et sérieux dans le monde professionnel. Vous y trouverez également certaine de mes qualités comme des défaut. Cette rubrique vous permettra de voir ma personnalité et quels sont mes centres d'intérêt hors du monde professionnel ainsi qu'une vidéo de présentation.<br /><a href="qui-suis-je.php" title="A propos de yoann" style="color: #82576b;"><strong>En savoir plus</strong></a></p>   
+            </div><br>
+          </div>
+          <br />
+          <div class="col-md-4">
+            <div class="col-md-12" style="background-color: #FFFEFE; border-width:2px; border-style:outset; border-radius: 15px;  box-shadow:10px 10px 20px 0px #696969;">
+              <div class="page-header" style="text-align: center;">
+                <h3 style="text-shadow: 5px 5px 6px #82576b;"><span class="glyphicon glyphicon-th-list"></span><strong>Mes expériences</strong></h3>
+              </div>
+              <br />
+              <img src="https://image.ibb.co/ddybs7/18.jpg" class="img-fluid" alt="wall street image">
+              <br /><br />
+              <p style="text-align: center;">Aux cours de cette catégorie vous trouverez mes différentes expériences comme le <strong>développement web</strong> et le métier de <strong>trader pour compte propre</strong>, mais aussi mes expériences de <strong>préparateur de commandes</strong> et de <strong>maçon</strong>. Vous y verrez des illustrations de mes expériences mais également une vidéo où je présente ce que j'ai effectué durant mes expériences.<br /><a href="experience.php" style="color: #82576b;"><strong>En savoir plus</strong></a></p>
+            </div>
+          </div>
+      </div>
+          <br />
+      <div class="col-md-12" style="background-color: #FFFEFE; border-width:2px; border-style:outset; border-radius: 15px;  box-shadow:10px 10px 20px 0px #696969;">
+          <div class="page-header" style="text-align: center;">
+            <h3 style="text-shadow: 5px 5px 6px #82576b;"><span class="glyphicon glyphicon-th-list"></span><strong>Mes projets</strong></h3>
+          </div>
+          <p style="text-align: center;">Les projets sont un élément clé dans ma vie et sont composées en deux catégories, il y a d'une par ma première passion pour la <strong>finance</strong> avec le projet de <strong>vivre du trading</strong>, mais aussi par ma passion de l'<stong>informatique</stong> où je vous présente plusieurs projets comme ce <strong>portfolio</strong> ou un <strong>site vitrine</strong>qui présente mes <strong>créations graphiques et web</strong>, il y a aussi le <strong>blog et les réseaux sociaux</strong> qui présente le coeur de mes expériences aux cours de ceux-ci, mais je vous présente également des articles et tutos sur les manières de sortir des revenu ou des compléments de revenu par les différents contrats comme en indépendant et les techniques du web comme les ptc, le minage de cryptomonnaie, etc. enfin le dernier projet qui est le plus conséquent est un site dynamique qui présente des formations dans le trading, mais aussi des analyses, des signaux, ect grâce à des espaces membres, premium, ect. Ce projet est mon premier gros projet dans le <strong>développement web</strong> et permet de confirmer toutes mes compétences dans le <strong>développement web</strong> mais également dans le <strong>trading</strong>. Il reprend donc bien mes deux passions de façon complet.<br /><a href="projetf.php" title="Yoann Contact" style="color: #82576b;"><strong>En savoir plus</strong></a></p>
+        </div>
+      <br />
+	</div>
+</div>
+	<?php include 'footer.php'; ?>
+</body>
+</html>
